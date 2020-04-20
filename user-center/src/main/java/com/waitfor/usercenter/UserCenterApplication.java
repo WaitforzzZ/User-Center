45d0b6aac702faaec2a3a131a276084e02bc5389
WaitforzzZ <1204918135@qq.com>
2020-04-20 11:38:17 +0800

    20200420_修改bug

--- user-center/src/main/java/com/waitfor/usercenter/UserCenterApplication.java
+++ user-center/src/main/java/com/waitfor/usercenter/UserCenterApplication.java
@@ -1,9 +1,7 @@
 package com.waitfor.usercenter;
 
-import com.waitfor.usercenter.rocketmq.MySink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import tk.mybatis.spring.annotation.MapperScan;
