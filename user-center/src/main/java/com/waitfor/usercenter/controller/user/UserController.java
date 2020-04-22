c2aadc441f36c84c91c4948514034e677cc80788
WaitforzzZ <1204918135@qq.com>
2020-04-22 16:01:17 +0800

    20200422_实现微信小程序登陆

--- user-center/src/main/java/com/waitfor/usercenter/controller/user/UserController.java
+++ user-center/src/main/java/com/waitfor/usercenter/controller/user/UserController.java
@@ -1,22 +1,80 @@
 package com.waitfor.usercenter.controller.user;
 
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import com.waitfor.usercenter.domain.dto.user.JwtTokenRespDTO;
+import com.waitfor.usercenter.domain.dto.user.LoginRespDTO;
+import com.waitfor.usercenter.domain.dto.user.UserLoginDTO;
+import com.waitfor.usercenter.domain.dto.user.UserRespDTO;
+import com.waitfor.usercenter.until.JwtOperator;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.waitfor.usercenter.domain.entity.user.User;
 import com.waitfor.usercenter.service.user.UserService;
 
+import java.util.HashMap;
+import java.util.Map;
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private WxMaService wxMaService;
+	@Autowired
+	private JwtOperator jwtOperator;
 	@GetMapping("/{id}")
 	public User findById(@PathVariable Integer id){
 		return this.userService.findById(id);
+	}
+
+	@PostMapping("/login")
+	public LoginRespDTO login(@RequestBody UserLoginDTO loginDTO) throws WxErrorException {
+		// 微信小程序服务端校验是否已经登录的结果
+		WxMaJscode2SessionResult result = this.wxMaService.getUserService()
+				.getSessionInfo(loginDTO.getCode());
+
+		// 微信的openId，用户在微信这边的唯一标示
+		String openid = result.getOpenid();
+
+		// 看用户是否注册，如果没有注册就（插入）
+		// 如果已经注册
+		User user = this.userService.login(loginDTO, openid);
+		// 颁发token
+		Map<String, Object> userInfo = new HashMap<>(3);
+		userInfo.put("id", user.getId());
+		userInfo.put("wxNickname", user.getWxNickname());
+		userInfo.put("role", user.getRoles());
+
+		String token = this.jwtOperator.generateToken(userInfo);
+
+		log.info(
+				"用户{}登录成功，生成的token = {}, 有效期到:{}",
+				loginDTO.getWxNickname(),
+				token,
+				jwtOperator.getExpirationTime()
+		);
+		// 构建响应
+		return LoginRespDTO.builder()
+				.user(
+						UserRespDTO.builder()
+								.id(user.getId())
+								.avatarUrl(user.getAvatarUrl())
+								.bonus(user.getBonus())
+								.wxNickname(user.getWxNickname())
+								.build()
+				)
+				.token(
+						JwtTokenRespDTO.builder()
+								.expirationTime(jwtOperator.getExpirationTime().getTime())
+								.token(token)
+								.build()
+				)
+				.build();
 	}
 }
--- user-center/src/main/java/com/waitfor/usercenter/service/user/UserService.java
+++ user-center/src/main/java/com/waitfor/usercenter/service/user/UserService.java
@@ -2,6 +2,7 @@ package com.waitfor.usercenter.service.user;
 
 import com.waitfor.usercenter.dao.bonus.BonusEventLogMapper;
 import com.waitfor.usercenter.domain.dto.messaging.UserAddBonusMsgDTO;
+import com.waitfor.usercenter.domain.dto.user.UserLoginDTO;
 import com.waitfor.usercenter.domain.entity.bonus.BonusEventLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,33 +20,57 @@ import java.util.Date;
 @Slf4j
 public class UserService {
 
-	private final UserMapper userMapper;
-	private final BonusEventLogMapper bonusEventLogMapper;
+    private final UserMapper userMapper;
+    private final BonusEventLogMapper bonusEventLogMapper;
 
-	public User findById(Integer id){
-		return this.userMapper.selectByPrimaryKey(id);
-	}
+    public User findById(Integer id) {
+        return this.userMapper.selectByPrimaryKey(id);
+    }
 
     @Transactional(rollbackFor = Exception.class)
-	public void addBonus(UserAddBonusMsgDTO msgDTO){
-		// 当收到消息的时候， 执行的业务
-		// 1. 为用户加积分
-		Integer userId = msgDTO.getUserId();
-		User user = this.userMapper.selectByPrimaryKey(userId);
-		Integer bonus = msgDTO.getBonus();
-		user.setBonus(user.getBonus()+ bonus);
-		this.userMapper.updateByPrimaryKey(user);
+    public void addBonus(UserAddBonusMsgDTO msgDTO) {
+        // 当收到消息的时候， 执行的业务
+        // 1. 为用户加积分
+        Integer userId = msgDTO.getUserId();
+        User user = this.userMapper.selectByPrimaryKey(userId);
+        Integer bonus = msgDTO.getBonus();
+        user.setBonus(user.getBonus() + bonus);
+        this.userMapper.updateByPrimaryKey(user);
 
-		// 2. 记录日志到bonus_event_log表里面
-		this.bonusEventLogMapper.insert(
-				BonusEventLog.builder()
-						.userId(userId)
-						.value(bonus)
-						.event("CONTRIBUTE")
-						.createTime(new Date())
-						.description("投稿加积分..")
+        // 2. 记录日志到bonus_event_log表里面
+        this.bonusEventLogMapper.insert(
+                BonusEventLog.builder()
+                        .userId(userId)
+                        .value(bonus)
+                        .event("CONTRIBUTE")
+                        .createTime(new Date())
+                        .description("投稿加积分..")
+                        .build()
+        );
+        log.info("积分添加完毕...");
+    }
+
+    public User login(UserLoginDTO loginDTO, String openid) {
+		User user = this.userMapper.selectOne(
+				User.builder()
+						.wxId(openid)
 						.build()
 		);
-		log.info("积分添加完毕...");
+		if(user == null){
+			User userToSave = User.builder()
+					.wxId(openid)
+					.bonus(300)
+					.wxNickname(loginDTO.getWxNickname())
+					.avatarUrl(loginDTO.getAvatarUrl())
+					.roles("user")
+					.createTime(new Date())
+					.updateTime(new Date())
+					.build();
+			this.userMapper.insertSelective(
+					userToSave
+			);
+			return userToSave;
+		}
+		return user;
 	}
 }
